0e66dd69f4595c58172c256ce11fc69ff91c9a01
Võ Hoàng Vũ <vuprotk21@gmail.com>
2021-11-18 16:40:57 +0700

    detail food layout and restaurant

---
+++ project/app/src/main/java/com/example/project/Activity/ShowFoodDetailActivity.java
@@ -0,0 +1,15 @@
+package com.example.project.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.project.R;
+
+public class ShowFoodDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_food);
+    }
+}
